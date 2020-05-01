76f66335a60ed3819bd81be00c96627497e26333
khalil Frough <khalil.froughkmu@gmail.com>
2020-05-01 20:54:18 +1000

    created all the users view and edited users controller wrote test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def new
     @user= User.new
   end
+
+  def index
+    @user = User.all
+  end 
  
   def show 
     @user = User.find(params[:id])
