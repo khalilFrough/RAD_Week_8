5cfb24885fd96f6bd28aba67acf63bcfc7d6bcfd
khalil Frough <khalil.froughkmu@gmail.com>
2020-05-01 15:12:36 +1000

    fixed the correct user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
     validates :name, presence: true, length: {maximum: 50}
-    validates :password, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6} #, allow_blank: true 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, 
             format:{ with: VALID_EMAIL_REGEX },
